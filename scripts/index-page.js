97723a4bfb56346dd3f7d58343ad60a29c2d7858
AndrewVillalba <andrewvillalba1@gmail.com>
2023-04-02 19:10:07 -0400

    updated styling for nav bar

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -3,15 +3,6 @@ const apiKey = "?api_key=dcb6d814-baad-466c-8bb0-e687accc3b55";
 const apiComments = "/comments"
 const apiShowdates = "/showdates"
 
-// const comments = [
-//     {name: `Connor Walton`, date: `02/17/2021`,
-//     text: `This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.`},
-//     {name: `Emilie Beach`, date: `01/09/2021`, 
-//     text: `I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.`},
-//     {name: `Miles Acosta`, date: `12/20/2020`, 
-//     text: `I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.`}
-// ]
-
 let comments = [];
 
 axios.get(api+apiComments+apiKey)
